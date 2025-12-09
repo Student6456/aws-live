4fc70b59e4ce946f9f0c1a2d5766582fc33da48a
Student6456 <teohyh-wm23@student.tarc.edu.my>
2025-12-09 15:26:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.c104w48o6gn9.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "BAIT3273"
+custompass = "teoh1234"
 customdb = "employee"
 custombucket = "teohyuhang1-employee"
 customregion = "us-east-1"
