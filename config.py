682e2d9f7c74672694d838bf9515fd4411856000
Student6456 <teohyh-wm23@student.tarc.edu.my>
2025-12-09 15:22:38 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c104w48o6gn9.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "BAIT3273"
+customdb = "employee"
+custombucket = "teohyuhang1-employee"
+customregion = "us-east-1"
+
